7f8086ce1007f66a0884045616e6211f0dd861bc
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-02-27 22:52:29 +0530

    added my_sqrt.c
    Program to create own square root function(with specific range).

---
+++ my_sqrt.c
@@ -0,0 +1,27 @@
+//Program to create own square root function(with specific range).
+#include<stdio.h>
+#include<conio.h>
+int mysqrt(int);
+int main()
+{
+    int num,res;
+    printf("Enter number\n");
+    scanf("%d",&num);
+    res=mysqrt(num);
+    printf("Square root is %d",res);
+    getch();
+    return 0;
+}
+int mysqrt(int x)
+{
+    int r;
+    if(x==1)
+        return 1;
+    else
+        {
+      r=x/2;
+      while(r*r>x)
+        r--;
+     return(r);
+    }
+}
