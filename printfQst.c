f8133373fd2a1d3b03d4f425b12fe43b292ca59a
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-05-29 12:43:38 +0530

    added printfQst.c

---
+++ printfQst.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+   int s;
+   printf("%d",printf("%s","Hello"));
+   s= printf("%d",44);
+   printf("\n%d",s);
+   printf("\n\n");
+   printf("%s\n","Hello");
+   printf("%10s","Hello");
+return 0;
+}
