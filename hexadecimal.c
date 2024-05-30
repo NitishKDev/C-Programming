b824ccad2ef80e38c24e16ee640fb44f6e7fb63b
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-05-30 11:10:55 +0530

    added hexadecimal.c

---
+++ hexadecimal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int ad=0x43FF; //assigning hexadecimal value to ad
+    printf("Decimal %d\n",ad);
+    printf("Hexadecimal %x\n",ad);
+    printf("Hexadecimal %X\n",ad);
+    return 0;
+}
+
+
